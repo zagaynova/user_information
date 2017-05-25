fac67b5fc249206f1b3153a8dae1cb84e5e4e48f
zagaynova <masha.zagaynova@gmail.com>
2017-05-25 16:27:22 +0200

    part 0 and part 1

--- app.js
+++ app.js
@@ -1,6 +1,78 @@
 var express = require( 'express' )
 var sayhellootje = require( './sayhello' )
 
+const pug = require('pug');
+var bodyParser = require('body-parser');
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+const compiledFunction = pug.compileFile('searchbar.pug');
+
+//Search for users
+var fs = require("fs");
+
+
+var users = JSON.parse( fs.readFileSync("users.json") );
+
+var responseString = "";
+for (var i = 0; i < users.length; i++) {
+	responseString += users[i].firstname + ", ";
+	responseString += users[i].lastname + ", ";
+	responseString += users[i].email + "<br>";
+}
+
+function findUser(name) {
+	var result = "";
+	for (var i = 0; i < users.length; i++) {
+		if (users[i].firstname == name || users[i].lastname == name) {
+			result += users[i].firstname + ", ";
+			result += users[i].lastname + ", ";
+			result += users[i].email + "<br>";
+		}
+	}
+	return result;
+}
+
+app.get('/route1', (request, response) => {
+    console.log('request.query is: ', request.query)
+	response.send(responseString)
+});
+
+app.get('/route2', (request, response) => {
+    console.log('request.query is: ', request.query)
+	response.send(compiledFunction())
+});
+
+app.post('/route3', (request, response) => {
+    console.log('request.query is: ', request.query)
+    console.log(request.body.name)
+	response.send(findUser(request.body.name))
+});
+
+const listener = app.listen(8080, () => {
+    console.log('server has started at ', listener.address().port)
+})
+
+/*
+// Compile the source code
+const compiledFunction = pug.compileFile('template.pug');
+
+
+// Render a set of data
+console.log(compiledFunction({
+  name: 'Timothy'
+}));
+// "<p>Timothy's Pug source code!</p>"
+
+// Render another set of data
+console.log(compiledFunction({
+  name: 'Forbes'
+}));
+// "<p>Forbes's Pug source code!</p>"
+
+
 //Search for users
 var fs = require("fs");
 var users = JSON.parse( fs.readFileSync("users.json") );
@@ -22,8 +94,9 @@ var nieuweGebruiker = {
 	"email": "ohho@mail.com"
 };
 users.push(nieuweGebruiker);
-var jsonString = JSON.stringify(users, null, "\n");
+var jsonString = JSON.stringify(users, null, "\t");
 fs.writeFile("toegevoegd.json", jsonString, 'utf8', writeSuccess);
 
 sayhellootje( process.argv[2] )// Hello, I am Peter
 
+*/
